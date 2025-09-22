e173857d446f42279fc03b41622e76fdfd8e67dd
Marharyta <nasty22kot@gmail.com>
2025-09-22 19:25:39 +0300

    changed file

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import habitRoutes from './routes/habits.js';
+import habitRoutes from '../backend/routes/habits.js';
 
 dotenv.config();
 
