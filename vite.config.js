10a9dbde6c3bb986a7a35771ed85a2709c2c99cd
Marharyta <nasty22kot@gmail.com>
2025-09-23 15:24:45 +0300

    Fixed html fiel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/habit-tracker/', // 👈 це ключовий рядок
+  base: '/habit-tracker-app/',
   plugins: [react()],
 });
