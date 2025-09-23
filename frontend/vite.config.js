0e9ba08c34f91f42d669d91f76b79b6d6d3f03ac
Marharyta <nasty22kot@gmail.com>
2025-09-23 15:30:07 +0300

    Fix vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/habit-tracker-app/',
+  base: '/habit-tracker-app/', // 👈 це ключовий рядок
   plugins: [react()],
 });
